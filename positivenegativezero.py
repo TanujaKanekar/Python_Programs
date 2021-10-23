4d71bff82696b55a7e9bfc11097cfc5128afdee8
Tanuja Kanekar <83006088+TanujaKanekar@users.noreply.github.com>
2021-10-23 15:38:30 +0530

    Add files via upload

---
+++ positivenegativezero.py
@@ -0,0 +1,12 @@
+#Program to check if a number is Positive,Negative or 0
+
+#Take input from the user 
+num=int(input("enter the number: "))
+
+#Check the following conditions to print the entered number is positive,negative or zero
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("zero")
+else:
+    print("Negative number")

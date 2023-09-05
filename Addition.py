b15e740db774ddc9fab47b370fb7b9b0e0dec1d9
Tanuja Kanekar <83006088+TanujaKanekar@users.noreply.github.com>
2023-09-05 21:24:03 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,11 @@
+# Program to add Two numbers
+
+num_1 = 2.1
+num_2 = 3.1
+
+#Adding the two numbers
+sum = num_1 + num_2
+
+#Printing the addition of two numbers
+print("The addition of {0} and {1} is {2}".format(num_1,num_2,sum))
+
---
+++ Addition_user_input.py
@@ -0,0 +1,11 @@
+#Program to add two numbers taking input from the user
+
+num_1 = input("Enter the first number: ")
+num_2 = input("Enter the second number: ")
+
+#Adding the two numbers
+sum = float(num_1) + float(num_2)
+
+#Displaying the result
+print("Sum of {0} and {1} is {2}".format(num_1,num_2,sum))
+
